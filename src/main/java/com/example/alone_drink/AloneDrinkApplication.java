a5d16010d8d469938c3d1710a3eb995961a87935
jh950925 <jh950925@naver.com>
2023-09-22 15:48:31 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/alone_drink/AloneDrinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.alone_drink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloneDrinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AloneDrinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alone_drink/AloneDrinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alone_drink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AloneDrinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
