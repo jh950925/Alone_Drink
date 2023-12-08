bd8be6b3da37f0693c30119ba2313faba02ac747
jh950925 <jh950925@naver.com>
2023-12-08 15:24:48 +0900

    삭제

--- src/main/java/com/example/alone_drink/repository/board/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.alone_drink.repository.board;
-
-import com.example.alone_drink.vo.board.entity.BoardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
-
-}
--- src/main/java/com/example/alone_drink/vo/board/entity/BoardEntity.java
+++ src/main/java/com/example/alone_drink/vo/board/entity/BoardEntity.java
@@ -1,58 +1,78 @@
 package com.example.alone_drink.vo.board.entity;
 
+import com.example.alone_drink.vo.user.entity.UserEntity;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @SuperBuilder
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-@Table(name = "tb_board")
+@ToString
+@Table(name = "TB_BOARD")
 public class BoardEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //id값이 null일 경우 자동
-    @Column(name = "BOARD_ID")
-    private Long boardId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("게시글번호")
+    @Column(length = 8, name = "BOARD_NO")
+    private Long boardNo;
 
-
-    @Column(name = "BOARD_TITLE")
+    @Comment("게시글명")
+    @Column(length = 100,nullable = false, name = "BOARD_TITLE")
     private String boardTitle;
 
-    @Column(name = "BOARD_CONT")
+    @Comment("게시글내용")
+    @Column(columnDefinition = "TEXT", name ="BOARD_CONT")
     private String boardCont;
 
-    @Column(name = "BOARD_WR_NM")
-    private String boardWrNm;
+    @Comment("게시글등록유저번호")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_NO")
+    private UserEntity userEntity;
 
-    @Column(name = "BOARD_VIEW")
-    private Long boardView;
+    @Comment("게시글댓글수")
+    @Column(name ="BOARD_COMT_CNT")
+    private int boardComtCnt;
 
-    @Column(name = "BOARD_WR_ID")
-    private String boardWrId;
+    @Comment("게시글조회수")
+    @Column(name ="BOARD_VIEW_CNT")
+    private int boardViewCnt;
 
-    @Column(name = "BOARD_REG_DT")
-    private String boardRegDt;
+    @Comment("첨부파일번호")
+    @Column(length = 20, name = "BOARD_FILE_NO")
+    private String boardFileNo;
 
-    @Column(name = "BOARD_MOD_DT")
-    private String boardModDt;
+    @Comment("등록일")
+    @CreatedDate
+    @Column(name = "REG_DT")
+    private Date regDt;
+    
+    @Comment("수정일")
+    @LastModifiedDate
+    @Column(name = "MOD_DT")
+    private Date modDt;
 
 
-    @Override
-    public String toString() {
-        return "BoardEntity{" +
-                "boardId=" + boardId +
-                ", boardTitle='" + boardTitle + '\'' +
-                ", boardCont='" + boardCont + '\'' +
-                ", boardWrNm='" + boardWrNm + '\'' +
-                ", boardView=" + boardView +
-                ", boardWrId='" + boardWrId + '\'' +
-                ", boardRegDt='" + boardRegDt + '\'' +
-                ", boardModDt='" + boardModDt + '\'' +
-                '}';
+    @Builder
+    public BoardEntity(
+            String boardTitle,
+            String boardCont,
+            int boardComtCnt,
+            int boardViewCnt,
+            String boardFileNo
+    ){
+        this.boardTitle = boardTitle;
+        this.boardCont = boardCont;
+        this.boardComtCnt = boardComtCnt;
+        this.boardViewCnt = boardViewCnt;
+        this.boardFileNo = boardFileNo;
     }
 }
