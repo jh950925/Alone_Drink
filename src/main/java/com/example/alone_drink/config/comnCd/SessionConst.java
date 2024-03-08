bed49c2891f7661e345369b6360020728e815ff4
jh950925 <jh950925@naver.com>
2024-03-08 14:08:06 +0900

    프로젝트 재설정

--- src/main/java/com/example/alone_drink/config/comnCd/SessionConst.java
+++
@@ -1,7 +0,0 @@
-package com.example.alone_drink.config.comnCd;
-
-public abstract class SessionConst {
-
-    public static final String USER_COOKIE = "userCookie"; // 사용자 쿠키 생성
-    
-}
--- src/main/java/com/example/alone_drink/repository/user/UserSnsInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.alone_drink.repository.user;
-
-import com.example.alone_drink.vo.user.entity.UserSnsEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSnsInfoRepository extends JpaRepository<UserSnsEntity, String> {
-}
