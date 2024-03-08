f161fdbbe817d12838e008dfc98a6e64c70c350c
jh950925 <jh950925@naver.com>
2024-03-08 17:01:25 +0900

    사용자 Rest 컨트롤러 생성

---
+++ src/main/java/com/example/alone_drink/controller/user/UserRestController.java
@@ -0,0 +1,9 @@
+package com.example.alone_drink.controller.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserRestController {
+}
