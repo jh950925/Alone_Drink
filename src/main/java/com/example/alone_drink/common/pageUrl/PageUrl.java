2189c723cfaa72b1d70892fbbecfe8c7d44bf6e0
jh950925 <jh950925@naver.com>
2024-03-26 14:48:40 +0900

    Page Url 폴더링 static 변수 선언

--- src/main/java/com/example/alone_drink/common/pageUrl/PageUrl.java
+++ src/main/java/com/example/alone_drink/common/pageUrl/PageUrl.java
@@ -2,9 +2,16 @@ package com.example.alone_drink.common.pageUrl;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ *  return page Path
+ */
 @Slf4j
 public class PageUrl {
 
+    // 유저화면
     public final static String PATH_VIEW_USER = "/user";
+
+    // 게시글 화면
+    public final static String PATH_VIEW_BOARD = "/board";
 
 }
