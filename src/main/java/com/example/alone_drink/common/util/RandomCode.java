a161ea999769d0cd7607d62c02836c97e65f35e7
jh950925 <jh950925@naver.com>
2024-05-27 15:58:16 +0900

    메일 랜덤코드

---
+++ src/main/java/com/example/alone_drink/common/util/RandomCode.java
@@ -0,0 +1,30 @@
+package com.example.alone_drink.common.util;
+
+import java.util.Random;
+
+public class RandomCode {
+
+    //랜덤 인증 코드 생성
+    public String createCode() {
+        Random random = new Random();
+        StringBuffer key = new StringBuffer();
+
+        for(int i=0;i<8;i++) {
+            int index = random.nextInt(3);
+
+            switch (index) {
+                case 0 :
+                    key.append((char) ((int)random.nextInt(26) + 97));
+                    break;
+                case 1:
+                    key.append((char) ((int)random.nextInt(26) + 65));
+                    break;
+                case 2:
+                    key.append(random.nextInt(9));
+                    break;
+            }
+        }
+        return key.toString();
+    }
+
+}
