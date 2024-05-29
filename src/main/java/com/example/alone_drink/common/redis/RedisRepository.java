cf9ddd783353c0cc5d1baca8e5045b1cccf4f8c5
jh950925 <jh950925@naver.com>
2024-05-29 13:15:23 +0900

    redis Jpa interface 사용

---
+++ src/main/java/com/example/alone_drink/common/redis/RedisRepository.java
@@ -0,0 +1,6 @@
+package com.example.alone_drink.common.redis;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RedisRepository extends CrudRepository<RedisVo, String> {
+}
